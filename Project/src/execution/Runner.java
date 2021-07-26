a02f1b9c3b661b5d884f44320e144a287f7a3b56
Jeimmy Valderrama <jeimmy.valderrama@uptc.edu.co>
2021-07-26 13:01:24 -0500

    Diferencia e Interseccion

--- Project/src/execution/Runner.java
+++ Project/src/execution/Runner.java
@@ -1,20 +1,112 @@
 package execution;
 import model.Methods;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
-import model.OwnName;
+import javax.swing.JOptionPane;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		Methods methods = new Methods();
 		
-		System.out.println(methods.probando());
-		System.out.println(methods.timesRepeatString());
-		System.out.println(methods.deleteChar());
+		//System.out.println(methods.probando());
+		//System.out.println(methods.timesRepeatString());
+
+		
+		
+		Scanner scanner=new Scanner(System.in); 
+		int option=0;
+		while (option!=9) {
+
+			System.out.println("Digite la opcion que desea realizar"
+					+ "\n 1.Nombre Propio"
+					+ "\n 2.Buscar Cadena "
+					+ "\n 3.Encriptar"
+					+ "\n 4.Desencriptar"
+					+ "\n 5 Llenar caracteres"
+					+ "\n 6.Borrar caracteres"
+					+"\n 7.Interseccion"
+					+"\n 8.Diferencia"
+					+"\n 9.Borrar caracteres"
+					+"\n 10.Validar direccion de correo"
+					+ "\n 11.Salir");
+			
+			System.out.println("\n   ");
+			
+			option=scanner.nextInt();
+			
+			switch (option) {
+
+			case 1:
+
+				System.out.println(methods.ownName());
+				System.out.println("\n   ");
+
+				break;
+				
+			case 2:
+
+				break;
+				
+				
+			case 3:
+			
+
+				break;
+				
+				
+			case 4:
+				
+				break;
+				
+			case 5:
+						
+				break;
+				
+			case 6:
+		
+				System.out.println(methods.deleteChar());
+				System.out.println("\n   ");
+				
+				break;
+				
+			case 7:
+				
+				Methods.showChain();
+				System.out.println("\n   ");
+				
+				break;
+				
+            case 8:
+	             
+            	Methods.Diference();
+            	System.out.println("\n   ");
+            	
+				break;
+				
+           case 9:
+	
+	
+	
+	           break;
+	           
+            case 10:
+	
+	
+	
+	         break;
+			case 11:
+				System.out.println("SALIENDO...");
+				System.exit(0);
+				break;
+
+			}
+		}
+		}
+		
 		
 
 	}
 
-}
+
--- Project/src/model/Methods.java
+++ Project/src/model/Methods.java
@@ -1,14 +1,46 @@
 package model;
 
+import java.util.Scanner;
+
 import model.Strings;
 
 public class Methods {
 
-	private Strings strings = new Strings();
+	public static Strings strings = new Strings();
 
 	String msj = strings.getMsj();
+	
+	//nombre propio+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	public  String ownName(){
+		
+		String chain = strings.getMsj();
+		char letterOne=' ';
+		char capitalLetter=' ';
+		String outcome=" ";
+		int counter=0;
+		int aux=0;
+		for (int i = 0; i < chain.length(); i++) {
+			letterOne=chain.charAt(0);
+			capitalLetter=chain.charAt(i);
+			if(i==0) {
+				capitalLetter=Character.toUpperCase(capitalLetter);
+			}if(capitalLetter==' ' || capitalLetter == chain.charAt(24)) {
+				counter=i+1;
+				
+			}if(counter==i) {
+				capitalLetter=Character.toUpperCase(capitalLetter);
+			}
+			outcome+=capitalLetter;
+		}
+		
+		return "Frase inicial:  " + chain
+			   +"\n Frase Final:   "+ outcome;
+      
+	}
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
 
-	public String probando() {
+	/*public String probando() {
 		String out = "";
 		char c = ' ';
 		int aux = 0;
@@ -33,8 +65,10 @@ public class Methods {
 			out += c;
 		}
 		return out;
-	}
-
+	}*/
+	
+	
+   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	public int timesRepeatString() {
 		String in = msj;
 		int count = 0;
@@ -46,7 +80,11 @@ public class Methods {
 		}
 		return count;
 	}
-
+	
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
+   
+	//borrar caracteres +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	public String deleteChar() {
 		String out = "";
 		char c = ' ';
@@ -59,6 +97,120 @@ public class Methods {
 			out+=c;
 		}
 		return out;
+		
 	}
+	//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
+	
+	//Interseccion +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
+	 public static void showChain() {
+			Scanner scanner=new Scanner(System.in);
+			System.out.println("Ingrese la primera cadena: ");
+			 String firstChain=new String (scanner.nextLine()).toLowerCase();
+
+			System.out.println("Ingrese la segunda cadena: ");
+			 String secondChain=new String (scanner.nextLine()).toLowerCase();
+				
+				String arrayTwo[]=secondChain.split("");
+				String arrayOne[]=firstChain.split("");
+				
+			   Methods.checkChain(arrayOne, arrayTwo);
+
+			}
+		public static void checkChain(String arrayOne[],String arrayTwo[]) {
+			
+				String message="";
+				String result="";
+				for (int i = 0; i < arrayOne.length; i++) {
+					
+					for (int j = 0; j < arrayTwo.length; j++) {
+						if(arrayOne[i].equals(arrayTwo[j])) {
+							message+=arrayTwo[j];
+						}
+					}
+					
+				}
+				
+				String arrayThree[]=message.split("");
+				for (int i = 0; i < arrayThree.length; i++) {
+					for (int j = i+1; j < arrayThree.length; j++) {
+						if(arrayThree[i].equals(arrayThree[j])) {
+							arrayThree[j]="";
+						}
+					}
+				}
+				
+				for (int i = 0; i < arrayThree.length; i++) {
+					result+=arrayThree[i];
+				}
+				System.out.println( "El resultado de la Interseccion es: " + result);
+          ;
+				
+			}
+			
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++	
+	
+
+//Diferencia+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+		public static void Diference() {
+			
+			Scanner scanner=new Scanner(System.in);
+			
+	        String firstChain= strings.getMsj() ;
+	         System.out.println("Digite la primera cadena ");
+	        String secondChain=new String (scanner.nextLine().toLowerCase());
+	         String arrayOne[]=firstChain.split("");
+	        String arrayTwo[]=secondChain.split("");
+	        
+	       
+	   
+	    
+	       System.out.println(delete(arrayOne.clone(),arrayTwo.clone()));
+	    	
+		}
+
+
+		public static String delete(String arrayOne[], String arrayTwo[] ) {
+			for (int i = 0; i < arrayOne.length; i++) {
+				boolean repeat=false;
+				for (int j = 0; j < arrayTwo.length; j++) {
+					if (arrayOne[i].equals(arrayTwo[j])) {
+						arrayOne[i]="";
+						repeat=true;
+						break;
+					}
+					if (repeat=false) {
+						break;
+					}
+					
+					
+				}
+			}
+			return fillChain(arrayOne);
+			
+		}
+		
+		public static  String fillChain(String[] arrayOne) {
+			String result=new String();
+			for (int i = 0; i < arrayOne.length; i++) {
+				result+=arrayOne[i];
+				
+			}
+			
+			
+			return "Diferencia:  " + result;
+		}
+		
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++		
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
