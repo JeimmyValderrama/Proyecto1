07e0116e6ced99c8938f4e813cf5e0ffac6e184b
Karen Hernández <86068762+Chipi30@users.noreply.github.com>
2021-08-22 21:28:53 -0500

    Add files via upload

--- Project/src/execution/Runner.java
+++ Project/src/execution/Runner.java
@@ -1,112 +1,31 @@
 package execution;
-import model.Methods;
-
-import java.util.Scanner;
 
 import javax.swing.JOptionPane;
+
+import model.OwnName;
 
 public class Runner {
 
 	public static void main(String[] args) {
-		Methods methods = new Methods();
 		
-		//System.out.println(methods.probando());
-		//System.out.println(methods.timesRepeatString());
-
+		String phrases = "";
+		String note = "sogamoso ciudad del sol y el acero";
+		OwnName nm = new OwnName(note);
+		nm.ownName();
 		
-		
-		Scanner scanner=new Scanner(System.in); 
-		int option=0;
-		while (option!=9) {
+        /**Encriptar cadena*/
+		phrases = JOptionPane.showInputDialog("escriba palabra u oracion a encriptar");
+		JOptionPane.showMessageDialog(null, nm.encrypt(phrases));
+		/**Desencriptar cadena*/
+		JOptionPane.showMessageDialog(null, nm.decrypt(phrases));
 
-			System.out.println("Digite la opcion que desea realizar"
-					+ "\n 1.Nombre Propio"
-					+ "\n 2.Buscar Cadena "
-					+ "\n 3.Encriptar"
-					+ "\n 4.Desencriptar"
-					+ "\n 5 Llenar caracteres"
-					+ "\n 6.Borrar caracteres"
-					+"\n 7.Interseccion"
-					+"\n 8.Diferencia"
-					+"\n 9.Borrar caracteres"
-					+"\n 10.Validar direccion de correo"
-					+ "\n 11.Salir");
-			
-			System.out.println("\n   ");
-			
-			option=scanner.nextInt();
-			
-			switch (option) {
-
-			case 1:
-
-				System.out.println(methods.ownName());
-				System.out.println("\n   ");
-
-				break;
-				
-			case 2:
-
-				break;
-				
-				
-			case 3:
-			
-
-				break;
-				
-				
-			case 4:
-				
-				break;
-				
-			case 5:
-						
-				break;
-				
-			case 6:
-		
-				System.out.println(methods.deleteChar());
-				System.out.println("\n   ");
-				
-				break;
-				
-			case 7:
-				
-				Methods.showChain();
-				System.out.println("\n   ");
-				
-				break;
-				
-            case 8:
-	             
-            	Methods.Diference();
-            	System.out.println("\n   ");
-            	
-				break;
-				
-           case 9:
-	
-	
-	
-	           break;
-	           
-            case 10:
-	
-	
-	
-	         break;
-			case 11:
-				System.out.println("SALIENDO...");
-				System.exit(0);
-				break;
-
-			}
-		}
-		}
-		
-		
+		/**Borar caracteres izquierda o derecha*/
+		String sentence = JOptionPane.showInputDialog("Digite texto: ");
+		String[] leftOrRigth = { "Derecha", "Izquierda"};
+		String orientation = Integer.toString(JOptionPane.showOptionDialog(null, "Por favor eliga por donde quiere empezar a borrar : ","Elegir dirección",
+				JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE, null, leftOrRigth, leftOrRigth[0]));
+		JOptionPane.showMessageDialog(null, nm.delete(sentence, orientation));
 
 	}
 
-
+}
